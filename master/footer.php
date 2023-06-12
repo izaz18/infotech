f28eef9865cbfb276c8528519aafac748c5d9f96
Izaz Kohli <126753119+izaz18@users.noreply.github.com>
2023-06-12 14:53:24 +0530

    Add files via upload

---
+++ master/footer.php
@@ -0,0 +1,125 @@
+<footer id="react-footer" class="react-footer home-main">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 md-mb-30">
+                    <div class="footer-widget footer-widget-1">
+                        <div class="footer-logo white">
+                            <a href="index.html" class="logo-text"> <img src="assets/images/logo-footer.png" alt="logo"
+                                    style="height: 87px;"></a>
+                        </div>
+                        <h6 class="footer-subtitle">Infotech Computer Education Centre (Infotech CEC)<br> is the fastest
+                            Growing Institute in Assam and its an ISO 9001:2015 Certified Institute and Registered under
+                            Societies Registration Act. XXI of 1860, Govt. of Assam, Ministry of Micro, Small & Medium
+                            Enterprises, Govt. of India and NITI Aayog, Govt. of India.</h6>
+                        <ul class="footer-address">
+                            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-phone">
+                                    <path
+                                        d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                    </path>
+                                </svg><a href="tel:+919706502126"> +91 9706502126 </a></li>
+                            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-phone">
+                                    <path
+                                        d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                    </path>
+                                </svg><a href="tel:+919365090176"> +91 9365090176 </a></li>
+                            <li><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-mail">
+                                    <path
+                                        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                                    </path>
+                                    <polyline points="22,6 12,13 2,6"></polyline>
+                                </svg><a href="mailto:infotechsipajhar2023@gmail.com"> infotechsipajhar2023@gmail.com
+                                </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-1">
+
+                </div>
+                <!-- <div class="col-lg-3 md-mb-30">
+                    <div class="footer-widget footer-widget-2">
+                        <h3 class="footer-title">About Us</h3>
+                        <div class="footer-menu">
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="coureses-grid.html">Courses</a></li>
+                                <li><a href="#">Events</a></li>
+                                <li><a href="#">Career</a></li>
+                                <li><a href="profile.html">Become a Teacher</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> -->
+                <div class="col-lg-7 md-mb-30">
+                    <div class="footer-widget footer-widget-3">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3573.972886281951!2d91.89493159999999!3d26.3920636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375aff49b1724719%3A0x17be77667a06fd1f!2sINFOTECH%20COMPUTER%20EDUCATION%20CENTRE%2C%20SIPAJHAR!5e0!3m2!1sen!2sin!4v1686462775706!5m2!1sen!2sin"
+                            width="560" height="350" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        <div class="container">
+            <div class="react-copy-left">© 2023 <a href="#">Infotech CEC.</a> All Rights Reserved</div>
+            <div class="react-copy-right"> Design & Developed by <a href="https://www.instagram.com/__iizaz__/"><span
+                        style="color: #d2093c">Izaz
+                        Kohli</span></a></div>
+        </div>
+    </div>
+</footer>
+<!--================= Footer Section End Here =================-->
+
+<!--================= Scroll to Top Start =================-->
+<div id="backscrollUp" class="home">
+    <span aria-hidden="true" class="arrow_carrot-up"></span>
+</div>
+<!--================= Scroll to Top End =================-->
+
+<!--================= Jquery latest version =================-->
+<script src="assets/js/jquery.min.js"></script>
+<!--================= Modernizr js =================-->
+<script src="assets/js/modernizr-2.8.3.min.js"></script>
+<!--================= Bootstrap js =================-->
+<script src="assets/js/bootstrap.min.js"></script>
+<!--================= Owl Carousel js =================-->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!--================= Swiper Slider js =================-->
+<script src="assets/js/swiper-bundle.min.js"></script>
+<!--================= Magnific Popup =================-->
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<!--================= Counter up js =================-->
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<!--================= Wow js =================-->
+<script src="assets/js/wow.min.js"></script>
+<!--================= menus js =================-->
+<script src="assets/js/menus.js"></script>
+<!--================= Plugins js =================-->
+<script src="assets/js/plugins.js"></script>
+<!--================= Main js =================-->
+<script src="assets/js/main.js"></script>
+
+<script>
+// dropdown show & hide
+$(".campus-dropdown-item > .showed").on("click", function() {
+    var element = $(this).parent(".campus-dropdown-item");
+    if (element.hasClass("active")) {
+        element.removeClass("active");
+    } else {
+        element.siblings(".campus-dropdown-item").removeClass('active');
+        element.addClass("active");
+    }
+});
+</script>
---
+++ master/header.php
@@ -0,0 +1,132 @@
+<head>
+    <meta charset="utf-8">
+    <title>Infotech Computer Centre</title>
+    <meta name="description" content="">
+    <!--================= Responsive Tag =================-->
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--================= Favicon =================-->
+    <link rel="apple-touch-icon" href="assets/images/fav.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/fav.png">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <!--================= Bootstrap V5 css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <!--================= Menus css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/menus.css">
+    <!--================= Animate css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!--================= Owl Carousel css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+    <!--================= Owl Carousel css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/swiper.min.css">
+    <!--================= Elegant icon css  =================-->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/elegant-icon.css">
+    <!--================= Fontawesome icon css  =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/all.min.css">
+    <!--================= Magnific Popup css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/magnific-popup.css">
+    <!--================= Animations css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/animations.css">
+    <!--================= style css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <!--================= Custom Spacing css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/custom-spacing.css">
+    <!--================= Responsive css =================-->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+</head>
+
+
+<!--================= Preloader Section Start Here =================-->
+<!-- <div id="react__preloader">
+        <div id="react__circle_loader"></div>
+        <div class="react__loader_logo"><img src="assets/images/preload.png" alt="Preload"></div>
+    </div> -->
+<!--================= Preloader Section End Here =================-->
+
+<!--================= Header Section Start Here =================-->
+<header id="react-header" class="react-header">
+    <div class="topbar-area style1">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6" style="margin: 0 66px 0 26px">
+                    <div class="topbar-contact">
+                        <ul>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-phone">
+                                    <path
+                                        d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                    </path>
+                                </svg>
+                                <a href="tel:+919706502126"> +91 9706502126
+                                </a>
+                            </li>
+                            <li>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-mail">
+                                    <path
+                                        d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                                    </path>
+                                    <polyline points="22,6 12,13 2,6"></polyline>
+                                </svg>
+                                <a href="mailto:infotechsipajhar2023@gmail.com">infotechsipajhar2023@gmail.com</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-5 text-right">
+                    <div class="toolbar-sl-share">
+                        <ul class="social-links">
+                            <li><a href="#"><span aria-hidden="true" class="social_facebook"></span></a></li>
+                            <li><a href="#"><span aria-hidden="true" class="social_twitter"></span></a></li>
+                            <li><a href="#"><span aria-hidden="true" class="social_linkedin"></span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="menu-part">
+
+        <div class="container">
+            <!--================= Menu Start Here =================-->
+            <div class="react-main-menu">
+                <nav>
+                    <!--================= Menu Toggle btn =================-->
+                    <div class="menu-toggle">
+                        <div class="logo">
+                            <a href="index.php" class="logo-text">
+                                <img src="assets/images/logos.png" style="height: 85px; margin-left: 22px;" alt="logo">
+
+                            </a>
+                        </div>
+                        <button type="button" id="menu-btn">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                    </div>
+                    <!--================= Menu Structure =================-->
+                    <div class="react-inner-menus">
+                        <ul id="backmenu" class="react-menus react-menus-six home react-sub-shadow">
+                            <li> <a href="index.php">Home</a></li>
+                            <li> <a href="about.php">About Us</a> </li>
+                            <li> <a href="courses.php">Courses</a> </li>
+                            <li> <a href="blog.php">Blog</a> </li>
+                            <li> <a href="gallery.php">Gallery</a> </li>
+                            <li> <a href="contact.php">Contact</a></li>
+                        </ul>
+
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
